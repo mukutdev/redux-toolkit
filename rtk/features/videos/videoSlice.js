b6fc75465db2c8b6aa8a75d072212f23bf320ad6
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-03-24 20:21:05 +0600

    video-slice-created

---
+++ rtk/features/videos/videoSlice.js
@@ -0,0 +1,41 @@
+const {createSlice, createAsyncThunk} = require("@reduxjs/toolkit")
+const fetch = require("node-fetch")
+
+const intialState = {
+    loading : false,
+    videos : {},
+    error : ""
+}
+
+const fetchVideos = createAsyncThunk(("post/fetchPost" , async()=>{
+    const response = await fetch("http://localhost:9000/videos")
+    const videos = await response.json()
+    return videos;
+}))
+
+
+const videoSlice = createSlice({
+    name : "videos",
+    intialState,
+    extraReducers : (builder)=>{
+        builder.addCase(fetchVideos.pending , (state , action)=>{
+            state.loading = true,
+            state.error = ''
+        })
+        builder.addCase(fetchVideos.fulfilled , (state , action)=>{
+            state.loading = false,
+            state.videos = action.payload
+            state.error = ''
+        })
+        builder.addCase(fetchVideos.rejected , (state , action)=>{
+            state.loading = false,
+            state.videos = {},
+            state.error = action.error.message
+        })
+
+    }
+})
+
+
+module.exports = videoSlice.reducer
+module.exports.fetchVideos = fetchVideos
